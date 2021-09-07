1cd08bd2220d9a750b5c11b6518cdfd72adab05f
Kanagat <k_sapiya@kbtu.kz>
2021-09-07 18:14:41 +0600

    D added

--- LAB1/d.cpp
+++ LAB1/d.cpp
@@ -10,16 +10,28 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-      
-    int n;
 
-    vector<int> v1;
-    vector<string> v2;
-    vector<pair<int, string>> v3;
+    int n; string s;
+    queue<pair<int,string>> nine, ten, eleven;
 
-    while (cin >> n){
-        
+    while (cin >> n >> s){
+        if (n == 9)  nine.push(make_pair(n,s));
+        if (n == 10) ten.push(make_pair(n,s));
+        if (n == 11) eleven.push(make_pair(n,s));
     }
-      
+
+    while (!nine.empty()){
+        cout << nine.front().first << sep << nine.front().second << '\n';
+        nine.pop();
+    }
+    while (!ten.empty()){
+        cout << ten.front().first << sep << ten.front().second << '\n';
+        ten.pop();
+    }
+    while (!eleven.empty()){
+        cout << eleven.front().first << sep << eleven.front().second << '\n';
+        eleven.pop();
+    }
+
     return 0;
 }
